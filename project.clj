c90da8cab6fe6bd1846d3192e62b46e732fcf882
Nils Grunwald <nils.grunwald@linkfluence.net>
2011-02-08 18:32:12 +0100

    not working yet

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
-(defproject clj-spore "1.0.0-SNAPSHOT"
+(defproject clj-spore "0.0.1-SNAPSHOT"
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [clj-http "0.1.3"]]
   :dev-dependencies [[swank-clojure "1.2.1"]])
--- src/clj_spore/generator.clj
+++ src/clj_spore/generator.clj
@@ -1,6 +1,6 @@
 (ns clj-spore.generator
-  (:use [clojure.contrib.str-utils :only [str-join]])
-  (:require [clojure.string :as str])
+  (:require [clojure.contrib.string :as str]
+            [clj-http.core :as c])
   (:import (java.net URL)))
 
 (def *api*)
@@ -16,12 +16,18 @@
   [response callbacks]
   response)
 
+(defn interpolate-params
+  [path params]
+  (reduce (fn [p [name val]] (str/replace-str (str name) val p)) path params))
+
 (defn finalize-request
-  [env]
-  env)
+  [{ path :PATH_INFO, params :spore.params :as env}]
+  (assoc env :PATH_INFO (interpolate-params path params)))
 
 (defn- send-request
-  [env callbacks])
+  [{ method :METHOD_NAME, host :SERVER_HOST, port :SERVER_PORT :as env} callbacks]
+  (println "sent")
+  (c/request {}))
 
 (defn- wrap-request
   [init-env middlewares]
@@ -36,40 +42,43 @@
 	  (recur (:env res)
 		 (rest mw)
 		 (if-let [cb (:cb res)] (conj callbacks cb) callbacks))))
-      (send-request (finalize-request env) callbacks))))
+      (send-request (finalize-request env) callbacks))
+    ))
 
 (defn generate-spore-method
   ([method_name spec]
-     (generate-spore-method *api* method_name spec)) 
-  ([{:keys [name author api_base_url api_format version]
+     (generate-spore-method *api* method_name spec))
+  ([{:keys [name author version], api_base_url :base_url, api_format :format
      :or {api_format []}
      :as api}
    method_name
    {:keys [method path params required expected description authentication base_url format documentation]
     :or {description (str method_name " method")
          authentication false
+         base_url api_base_url
+         format (or api_format [])
 	 params []
 	 required []
 	 expected []
-         format []
          documentation (str "no documentation for " method_name)}
     :as spec }]
      (fn
        [& {:as user-params}]
-       (let [missing (check-missing-params required user-params)
-	     base (or base_url api_base_url)
-	     base_uri (URL. base)
-	     env {:METHOD_NAME method
-		  :SCRIPT_NAME (.getPath base_uri)
-		  :PATH_INFO path
-		  :REQUEST_URI ""
-		  :SERVER_HOST (.getHost base_uri)
-		  :SERVER_PORT (.getPort base_uri) 
-		  :QUERY_STRING ""
-		  :spore.payload (:payload user-params)
-		  :spore.params (dissoc user-params :payload)
-		  :spore.scheme (.getProtocol base_uri)}]
-	 (if-not (empty? missing)
-	   (Response. 599 {:error (str "missing params calling " method_name ": " (str/join ", " missing))})
-	   (wrap-request env *middlewares*)
-	 )))))
+       (let [missing (check-missing-params required user-params)]
+         (if-not (empty? missing)
+           nil
+           ;; (Response. 599 {:error (str "missing params calling " method_name ": " (str/join ", " missing))})
+           (let [base_uri (URL. base_url)
+                 env {:METHOD_NAME method
+                      :SCRIPT_NAME (.getPath base_uri)
+                      :PATH_INFO path
+                      :REQUEST_URI ""
+                      :SERVER_HOST (.getHost base_uri)
+                      :SERVER_PORT (.getPort base_uri) 
+                      :QUERY_STRING ""
+                      :spore.payload (:payload user-params)
+                      :spore.params (dissoc user-params :payload)
+                      :spore.scheme (.getProtocol base_uri)}]
+;             env
+             (wrap-request env *middlewares*)
+             ))))))
