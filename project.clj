05d32ef50af14d2988efd8dca1e9c33129ce5b01
Nils Grunwald <nils@grunwald.fr>
2012-01-10 16:31:05 +0100

    updated clj-http dependency

--- project.clj
+++ project.clj
@@ -3,4 +3,4 @@
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [clj-json "0.3.1"]
-                 [clj-http "0.1.3"]])
+                 [clj-http "0.2.5"]])
