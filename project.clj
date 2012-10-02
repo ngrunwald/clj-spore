643087277b0b391ebc749cc4344203d0d85a6202
Nils Grunwald <nils@grunwald.fr>
2012-10-02 12:07:37 +0200

    bump version and deps

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
-(defproject clj-spore "0.0.2-SNAPSHOT"
+(defproject clj-spore "0.0.2"
   :description "SPORE client implementation for the Clojure language"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [cheshire "4.0.0"]
-                 [clj-http "0.4.3"]
-                 [slingshot "0.10.3"]]
-  :dev-dependencies [[clj-stacktrace "0.2.4"]])
+                 [cheshire "4.0.3"]
+                 [clj-http "0.5.5"]
+                 [slingshot "0.10.3"]])
